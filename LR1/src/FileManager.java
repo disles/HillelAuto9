69529b77cd06ae8261e9ac821f34d71ead63ccc4
original <zyxellen@gmail.com>
2017-03-01 20:00:28 +0200

    added First LR

---
+++ LR1/src/FileManager.java
@@ -0,0 +1,22 @@
+public class FileManager {
+//    public void createFile(){
+//
+//    }
+//    public void deleteFile(){
+//
+//    }
+//
+//    public void renameFile(){
+//
+//    }
+//
+//    public void findWord(){
+//
+//    }
+//
+//    public void replaceWord(){
+//
+//    }
+
+    public String 
+}
---
+++ LR1/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by original on 27.02.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("1. Create File");
+        System.out.println("1. Delete File");
+    }
+
+}
