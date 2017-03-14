57ea75b46ae63ec147bcd00c8a4823ea5d793f58
disles <zyxellen@gmail.com>
2017-03-14 22:08:37 +0200

    moved files to LR3 folder

--- LR4_practice/src/main/java/Main.java
+++ LR4_practice/src/main/java/Main.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
+
 /**
  * Created by original on 13.03.2017.
  */
 public class Main {
     public static void main(String[] args) {
         HillelFileWriter writer = new HillelFileWriter();
-        writer.writeToFile()
+        if (args.length == 2){
+            writer.writeToFile(args[0],args[1]);
+        }
+        else if (args.length == 0){
+            Scanner scanner = new Scanner(System.in);
+
+        }
+        else {
+            errorMessage();
+        }
+    }
+    public static void errorMessage(){
+        System.out.println("command line usage: java -jar write.jar <filePath> <text>");
+        System.out.println("interactive: java -jar write.jar");
     }
 }
